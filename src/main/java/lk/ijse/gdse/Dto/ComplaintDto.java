db6a3b8ddbcab711bb02f22426e7f0b9a39564b7
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-17 16:02:58 +0530

    Create complaintDto

---
+++ src/main/java/lk/ijse/gdse/Dto/ComplaintDto.java
@@ -0,0 +1,67 @@
+package lk.ijse.gdse.Dto;
+
+public class ComplaintDto {
+     private String cid, uname, subject, description, date , status;
+
+
+    public ComplaintDto() {
+
+    }
+
+    public ComplaintDto(String uname, String cid, String subject, String description, String date, String status) {
+        this.uname = uname;
+        this.cid = cid;
+        this.subject = subject;
+        this.description = description;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

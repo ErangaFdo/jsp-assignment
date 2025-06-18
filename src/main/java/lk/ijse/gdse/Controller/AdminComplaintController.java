2416e43c6f00e23aedf8f5f361cf0e3f2c82a64c
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-18 15:39:13 +0530

    design ComplaintModel

---
+++ src/main/java/lk/ijse/gdse/Controller/AdminComplaintController.java
@@ -0,0 +1,32 @@
+package lk.ijse.gdse.Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse.Model.CompliantModel;
+
+import java.io.IOException;
+
+@WebServlet("/admincomplaint")
+public class AdminComplaintController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         String action = req.getParameter("action");
+        CompliantModel compliantModel = new CompliantModel();
+
+        switch (action) {
+            case "update":
+                String cid = req.getParameter("cid");
+                String username = req.getParameter("uname");
+                String subject = req.getParameter("subject");
+                String date = req.getParameter("date");
+                String description = req.getParameter("description");
+                String status = req.getParameter("status");
+
+                boolean update = compliantModel.
+        }
+
+    }
+}
